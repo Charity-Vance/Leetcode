c7c4a1e7d0da1621ba30935e7c6abe12b1f80bd2
Charity-Vance <68844691+Charity-Vance@users.noreply.github.com>
2021-09-14 16:24:41 +0800

    Update 2、通过删除字母匹配到字典里最长单词-题解2(排序+双指针).java

--- 210914通过删除字母匹配到字典里最长单词524(双指针、动态规划)/2、通过删除字母匹配到字典里最长单词-题解2(排序+双指针).java
+++ 210914通过删除字母匹配到字典里最长单词524(双指针、动态规划)/2、通过删除字母匹配到字典里最长单词-题解2(排序+双指针).java
@@ -1,1 +1,32 @@
 
+class Solution {
+    public String findLongestWord(String s, List<String> dictionary) {
+        Collections.sort(dictionary, new Comparator<String>() {
+            public int compare(String word1, String word2) {
+                if (word1.length() != word2.length()) {
+                    return word2.length() - word1.length();
+                } else {
+                    return word1.compareTo(word2);
+                }
+            }
+        });
+        for (String t : dictionary) {
+            int i = 0, j = 0;
+            while (i < t.length() && j < s.length()) {
+                if (t.charAt(i) == s.charAt(j)) {
+                    ++i;
+                }
+                ++j;
+            }
+            if (i == t.length()) {
+                return t;
+            }
+        }
+        return "";
+    }
+}
+
+作者：LeetCode-Solution
+链接：https://leetcode-cn.com/problems/longest-word-in-dictionary-through-deleting/solution/tong-guo-shan-chu-zi-mu-pi-pei-dao-zi-di-at66/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
